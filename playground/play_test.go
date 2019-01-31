5bfb95611f60451b129ac0cdff8c85216e56b947
mark <mafuqiang@outlook.com>
2019-01-31 23:56:28 +0800

    playground init add file watcher gofmt

--- playground/play_test.go
+++ playground/play_test.go
@@ -2,10 +2,21 @@ package playground
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
 )
 
 func TestPlay(tt *testing.T) {
+
+	sliceCopyWillBeValueAnother()
+
+	goDeferMistake()
+
+	getCurrentInfo()
+
+}
+
+func sliceCopyWillBeValueAnother() {
 	var sl = []int{1, 2, 3, 4, 5, 6}
 	var sl2 = make([]int, 3)
 
@@ -13,13 +24,26 @@ func TestPlay(tt *testing.T) {
 
 	sl2[2] = 9
 	fmt.Println(sl, sl2)
+}
 
+func goDeferMistake() {
 	var i = 1
-	defer func() {
+
+	// second call defer ⬆️⬆️
+	defer fmt.Println("result: ", func() int { return i * 2 }()) // not func(){}()  inline reserve vars directly
+
+	// first call defer ⬆️
+	defer func() { // call func delay also vars calculated delay
 		fmt.Println("result: ", func() int {
 			return i * 2
 		}())
 	}()
 	i++
+}
 
+func getCurrentInfo() {
+	cpu := runtime.NumCPU()
+	goroutine := runtime.NumGoroutine()
+	call := runtime.NumCgoCall()
+	fmt.Println(cpu, goroutine, call)
 }
