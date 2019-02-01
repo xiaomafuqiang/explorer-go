7751d2e671771604d9747929f83fa48c642646ee
mafuqiang <mafuqiang@outlook.com>
2019-02-01 17:22:10 +0800

    http demo init

---
+++ playground/alwaysDel/mul_test.go
@@ -0,0 +1,48 @@
+package alwaysDel
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+type Car struct {
+	Name string
+	Age  int
+}
+
+func (c *Car) Set(name string, age int) {
+	c.Name = name
+	c.Age = age
+}
+
+type Car2 struct {
+	Name string
+}
+
+//Go有匿名字段特性
+type Train struct {
+	Car
+	Car2
+	createTime time.Time
+	//count int   正常写法，Go的特性可以写成
+	int
+}
+
+//给Train加方法，t指定接受变量的名字，变量可以叫this，t，p
+func (t Train) Set(age int) {
+	t.int = age
+}
+
+func TestIt(t *testing.T) {
+	//var train Train
+	//train.int = 300 //这里用的匿名字段写法，给Age赋值
+	////(&train).Set(1000)
+	//train.Car.Set("huas", 100)
+	//train.Car.Name = "test" //这里Name必须得指定结构体
+	//fmt.Println(train)
+
+	var train Train
+	fmt.Println(train.int)
+
+}
