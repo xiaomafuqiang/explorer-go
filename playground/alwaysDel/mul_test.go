ce1689a4eac394255f43fe9e90d35ae52d7d7381
mark <mafuqiang@outlook.com>
2019-02-02 07:53:35 +0800

    playground init add file watcher gofmt

--- playground/alwaysDel/mul_test.go
+++ playground/alwaysDel/mul_test.go
@@ -46,3 +46,33 @@ func TestIt(t *testing.T) {
 	fmt.Println(train.int)
 
 }
+
+type data struct {
+	name string
+}
+
+type printer interface {
+	print()
+}
+
+func (p *data) print() {
+	fmt.Println("name: ", p.name)
+	p.name = "new data..."
+}
+
+func TestIts(t *testing.T) {
+	d1 := data{"one"}
+	d1.print() // d1 变量可寻址，可直接调用指针 receiver 的方法
+
+	var in printer = &data{"two"}
+	in.print() // 类型不匹配
+
+	m := map[string]data{
+		"x": data{"three"},
+	}
+	d := m["x"]
+	d.print()
+	fmt.Println(d)
+	m["x"].print() // m["x"] 是不可寻址的	// 变动频繁
+
+}
